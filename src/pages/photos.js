66adcdf9eb13cd5bdfd1d7b8e5bac84cb050c1b5
tcbegley <tomcbegley@gmail.com>
2021-04-10 10:11:54 +0100

    Better photo gallery

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,51 +1,58 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Gallery from 'react-grid-gallery'
 import SEO from '../components/seo'
 import Layout from '../components/layout'
 import style from '../styles/post.module.css'
 import '../styles/photos.css'
 
-export default ({ data }) => (
-  <>
-    <SEO title={'Photos'} />
-    <Layout>
-      <div className={style.post}>
-        <div className={style.postContent}>
-          <h1 className={style.title}>Photos</h1>
-          <p>
-            I'm a keen amateur photographer. On this page you can find a few of
-            the photos I've taken over the years.
-          </p>
-          <Carousel dynamicHeight showIndicators={false}>
-            {data.allFlickrPhoto.edges.map(edge => (
-              <div style={{ height: '500px' }}>
-                <img
-                  src={edge.node.url_c}
-                  style={{ height: 'auto', width: 'auto' }}
-                />
-                <p className="legend">{edge.node.description}</p>
-              </div>
-            ))}
-          </Carousel>
-          All of these photos are also available on my{' '}
-          <a href="https://flickr.com/photos/149210668@N06/">Flickr</a> account.
+export default ({ data }) => {
+  const images = data.allFlickrPhoto.nodes.map(node => ({
+    src: node.url_l,
+    thumbnail: node.url_s,
+    thumbnailWidth: node.width_s,
+    thumbnailHeight: node.height_s,
+    caption: node.description,
+  }))
+  return (
+    <>
+      <SEO title={'Photos'} />
+      <Layout>
+        <div className={style.post}>
+          <div className={style.postContent}>
+            <h1 className={style.title}>Photos</h1>
+            <p>
+              I'm a keen amateur photographer. On this page you can find a few
+              of the photos I've taken over the years. Click on any of them to
+              see a higher resolution version.
+            </p>
+            <p>
+              All of these photos are also available on my{' '}
+              <a href="https://flickr.com/photos/149210668@N06/">Flickr</a>{' '}
+              account.
+            </p>
+            <Gallery
+              id="photo-gallery"
+              enableImageSelection={false}
+              images={images}
+            />
+          </div>
         </div>
-      </div>
-    </Layout>
-  </>
-)
+      </Layout>
+    </>
+  )
+}
 
 export const query = graphql`
   query {
     allFlickrPhoto {
-      edges {
-        node {
-          id
-          url_c
-          description
-        }
+      nodes {
+        id
+        width_s
+        height_s
+        url_s
+        url_l
+        description
       }
     }
   }
